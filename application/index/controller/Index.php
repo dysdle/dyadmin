0c0999be5580249c59c60ff0efd827831456fd30
dingye <dingye@shein.com>
2018-10-10 09:30:03 +0800

    no message

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,9 +1,21 @@
 <?php
 namespace app\index\controller;
+use think\facade\Env;
+use think\facade\Config;
 
 class Index{
     public function index(){
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
+        //return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
+        echo Env::get('APP_DEBUG');
+        //print_r(Config::get('app.'));
+        //var_dump(Config::pull('app'));
+        //print_r(Config::get());
+        //echo Config('default_lang');
+//        Config::set(['username'=>'123','password'=>'456'],'app');
+//        echo Config::get('username');
+//        echo Config::get('username');
+//        echo $_SERVER['PATH_INFO'];
+//        echo $_SERVER['REQUEST_URI'];
     }
 
     public function hello($name = 'ThinkPHP5'){
